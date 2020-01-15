cca2c03f78ff88750c971394ad1517df1c56a081
E-Fv1 <theedisonfuh@gmail.com>
2020-01-14 16:50:43 -0800

    sh: wq: command not found init master

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -15,21 +15,11 @@
 #include <ctre/Phoenix.h>
 #include <frc/Joystick.h>
 
-#include "rev/ColorSensorV3.h"
-
 namespace apesofwrath {
 
-enum Colors {
-  RED = 0,
-  BLUE = 1,
-  YELLOW = 2,
-  GREEN = 3,
-  WHITE = 4
-};
 class Robot : public frc::TimedRobot {
  public:
 
-  Colors currentColor, desiredColor;
   frc::Joystick* joy;
   TalonSRX* talon0;
 
@@ -43,20 +33,13 @@ class Robot : public frc::TimedRobot {
   void TeleopInit() override;
   void TeleopPeriodic() override;
   void TestPeriodic() override;
-  std::string getColor(apesofwrath::Colors c);
 
-
-  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
-  rev::ColorSensorV3 m_colorSensor{i2cPort};
-  frc::Color detectedColor;
   
  private:
   frc::SendableChooser<std::string> m_chooser;
-  frc::SendableChooser<Colors> m_descolor_chooser;
   
   const std::string kAutoNameDefault = "Default";
   const std::string kAutoNameCustom = "My Auto";
-  const Colors kDesColorDefault = Colors::WHITE;
   std::string m_autoSelected;
 };
 
