5a4db22a83be744b569826035b414f75e70d8e29
superspeeder101 <andy@delusion.org>
2020-02-15 17:59:59 -0800

    added more to the states

--- src/main/cpp/TeleopStateMachine.cpp
+++ src/main/cpp/TeleopStateMachine.cpp
@@ -4,8 +4,14 @@ using namespace frc;
 
 TeleopStateMachine::TeleopStateMachine(/* args */)
 {
-    state = WAIT_FOR_BUTTON;
-    last_state = WAIT_FOR_BUTTON;
+    state = WAIT_FOR_BUTTON_STATE;
+    last_state = WAIT_FOR_BUTTON_STATE;
+
+    bool state_arm = false;
+    bool state_intake = false;
+    bool state_shooter = false;
+    bool state_control_panel = false;
+
 }
 
 TeleopStateMachine::~TeleopStateMachine(){
@@ -20,6 +26,7 @@ ButtonData TeleopStateMachine::GatherButtonDataFromJoysticks(Joystick* joyThrott
         joyOp->GetRawButton(ButtonIDs::MANUAL_CONTROL_PANEL_BUTTON),
         joyOp->GetRawButton(ButtonIDs::ROTATION_MODE_CONTROL_PANEL_BUTTON),
         joyOp->GetRawButton(ButtonIDs::POSITION_MODE_CONTROL_PANEL_BUTTON),
+        joyOp->GetRawButton(ButtonIDs::SHOOT_BUTTON)
      };
 }
 
@@ -28,7 +35,7 @@ void TeleopStateMachine::ProcessButtonData(ButtonData data) {
         state = WAIT_FOR_BUTTON_STATE;
     } else if (data.intake_button) {
         state = INTAKE_STATE;
-    } else if (data.shooter_shoot_button) {
+    } else if (data.shoot_button) {
         state = SHOOT_STATE;
     } else if (data.rotation_mode_control_panel_button) {
         state = ROTATION_MODE_CONTROL_PANEL;
@@ -41,21 +48,116 @@ void TeleopStateMachine::StateMachine(ButtonData data) {
     ProcessButtonData(data);
 
     switch (state) {
+
+
         case INIT_STATE:
-          arm->intake_arm_state = arm->REST_STATE;
-          intake->intake_state = intake->STOP_STATE;
-          shooter->shooter_state = shooter->STOP_STATE;
-          control_panel->control_panel_state = control_panel->IDLE;
+          arm->intake_arm_state = arm->REST_STATE_H;
+          intake->intake_state = intake->STOP_STATE_H;
+          shooter->shooter_state = shooter->STOP_STATE_H;
+          control_panel->control_panel_state = control_panel->IDLE_STATE_H;
+
+          state = WAIT_FOR_BUTTON_STATE;
         break;
+
+
         case WAIT_FOR_BUTTON_STATE:
+          if (data.intake_button) {
+            state = INTAKE_STATE;
+          } else if (data.shoot_button) {
+            state = SHOOT_STATE;
+          } else if (data.rotation_mode_control_panel_button) {
+            state = ROTATION_MODE_CONTROL_PANEL_STATE;
+          } else if (data.position_mode_control_panel_button) {
+            state = POSITION_MODE_CONTROL_PANEL_STATE;
+          }
+
+          last_state = WAIT_FOR_BUTTON_STATE
         break;
+
+
         case INTAKE_STATE:
+          if (state_arm) {
+          arm->intake_arm_state = arm->DOWN_STATE_H;
+          }
+          if (state_intake) {
+          intake->intake_state = intake->IN_STATE_H;
+          }
+          if (state_shooter) {
+          shooter->shooter_state = shooter->INTAKE_STATE_H;
+          }
+
+          state = POST_INTAKE_STATE;
+          last_state = INTAKE_STATE;
         break;
+
+
+        case POST_INTAKE_STATE:
+          if (state_arm) {
+          arm->intake_arm_state = arm->UP_STATE_H;
+          }
+          if (state_intake) {
+          intake->intake_state = intake->STOP_STATE_H;
+          }
+          if (state_shooter) {
+          shooter->shooter_state = shooter->WAITING_STATE_H;
+          }
+
+          if (data.shoot_button) {
+            state = SHOOT_STATE;
+          }
+          last_state = POST_INTAKE_STATE;
+        break;
+
+
         case SHOOT_STATE:
+          if (state_arm) {
+          arm->intake_arm_state = arm->REST_STATE_H;
+          }
+          if (state_intake) {
+          intake->intake_state = intake->STOP_STATE_H;
+          }
+          if (state_shooter) {
+          shooter->shooter_state = shooter->SHOOT_STATE_H;
+          }
+
+          state = POST_SHOOT_STATE;
+          last_state = SHOOT_STATE;
         break;
+
+
+        case POST_SHOOT_STATE:
+          if (state_arm) {
+          arm->intake_arm_state = arm->REST_STATE_H;
+          }
+          if (state_intake) {
+          intake->intake_state = intake->STOP_STATE_H;
+          }
+          if (state_shooter) {
+          shooter->shooter_state = shooter->STOP_STATE_H;
+          }
+
+          state = WAIT_FOR_BUTTON_STATE;
+          last_state = POST_SHOOT_STATE;
+        break;
+
+
         case ROTATION_MODE_CONTROL_PANEL_STATE:
+          if (state_control_panel) {
+          control_panel->control_panel_state = control_panel->ROTATION_MODE_H;
+          }
+
+          state = WAIT_FOR_BUTTON_STATE;
+          last_state = ROTATION_MODE_CONTROL_PANEL_STATE;
         break;
+
+
         case POSITION_MODE_CONTROL_PANEL_STATE:
+          if (state_control_panel) {
+          control_panel->control_panel_state = control_panel->POSITION_MODE_H;
+          }
+
+          state = WAIT_FOR_BUTTON_STATE;
+          last_state = POSITION_MODE_CONTROL_PANEL_STATE;
         break;
     }
 
@@ -72,9 +174,13 @@ std::string TeleopStateMachine::StateName(TeleopStateMachine::States state) {
         case WAIT_FOR_BUTTON_STATE:
             return "Robot::Wait For Button";
         case INTAKE_STATE:
-            return "Intake::";
+            return "Robot::Intake";
+        case POST_INTAKE_STATE:
+            return "Robot::PostIntake";
         case SHOOT_STATE:
             return "Shooter::Shoot";
+        case POST_SHOOT_STATE:
+            return "Shooter::PostShoot";
         case ROTATION_MODE_CONTROL_PANEL:
             return "Control Panel::Rotation Mode";
         case POSITION_MODE_CONTROL_PANEL:
